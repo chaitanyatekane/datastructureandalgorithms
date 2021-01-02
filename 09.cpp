78d56e4c47a7b7b4bf117710de35cfdc69937890
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-02 10:24:28 +0530

    Write a program to print all numbers from 1 to 100 which are not divisible by 3

---
+++ 09.cpp
@@ -0,0 +1,18 @@
+// Write a program to print all numbers from 1 to 100 which are not divisible by 3.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int i = 0; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+        {
+            continue;
+        }
+        cout << i << endl;
+    }
+    return 0;
+}
