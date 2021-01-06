e701c2dee4275110382e521cdc03add2436c9501
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-06 17:49:48 +0530

    Write a program to print the ASCII value of any given character

---
+++ 18.cpp
@@ -0,0 +1,12 @@
+// Write a program to print the ASCII value of any given character.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout<<"Enter Any Character :- ";
+    cin>>c;
+    cout<<"ASCII value of "<<c<<" is "<<(int) c;
+    return 0;
+}
