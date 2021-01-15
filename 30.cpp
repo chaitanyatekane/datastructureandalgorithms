9bd366a9d181f62490793d5cc6f2c22594b210c0
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-15 07:41:26 +0530

    Print pyramid pattern using numbers (pattern questions)

---
+++ 30.cpp
@@ -0,0 +1,32 @@
+// Print pyramid pattern using numbers (pattern questions)
+
+/*
+n=5
+
+    1 
+   1 2 
+  1 2 3 
+ 1 2 3 4 
+1 2 3 4 5 
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Value of n :- ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
