3544e604f38c17b741131f6c8037dae837bf3432
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-15 07:40:13 +0530

    Print inverted half pyramid pattern using numbers (pattern questions)

---
+++ 27.cpp
@@ -0,0 +1,29 @@
+// Print inverted half pyramid pattern using numbers (pattern questions)
+
+/* 
+n=5
+
+1 2 3 4 5 
+1 2 3 4 
+1 2 3
+1 2 
+1
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Value of n :- ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n+1-i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
