a996b7a48a8a71b16bc06bee689dcbf99f9810e6
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-16 21:08:36 +0530

    Write a program to print Fibonacci sequence

---
+++ 35.cpp
@@ -0,0 +1,26 @@
+//  Write a program to print Fibonacci sequence.
+#include<iostream>
+using namespace std;
+
+void fib(int n){
+    int t1=0;
+    int t2=1;
+    int nextTerm;
+
+    for(int i=1; i<=n; i++){
+        cout<<t1<<endl;
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    return;
+}
+
+int main(){
+    int n;
+    cout<<"Enter How Many Terms You Want To Display Sequence Of :- ";
+    cin>>n;
+
+    fib(n);
+    return 0;
+}
