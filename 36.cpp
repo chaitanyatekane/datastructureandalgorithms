c107403e082e191bf47315e399c9eb4278625474
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-16 21:09:07 +0530

    Write a program to find the factorial of a given number

---
+++ 36.cpp
@@ -0,0 +1,22 @@
+// Write a program to find the factorial of a given number.
+
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    int factorial = 1;
+    for(int i=2; i<=n; i++){
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+
+int main(){
+    int n;
+    cout<<"Enter Number Of Which You Want Factorial Of :- ";
+    cin>>n;
+
+    int ans = fact(n);
+    cout<<ans<<endl;
+    return 0;
+}
