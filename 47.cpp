ca44293ba47b6fee0948323a3638615a82a6a006
Tekane Chaitanya Santosh <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-24 15:02:06 +0530

    Write a program to convert octal number to decimal number

---
+++ 47.cpp
@@ -0,0 +1,25 @@
+// Write a program to convert octal number to decimal number
+
+#include<iostream>
+using namespace std;
+
+int octalToDecimal(int n){
+    int ans = 0;
+    int x=1;
+    while(n>0){
+        int y=n%10;
+        ans+=x*y;
+        x*=8;
+        n/=10;
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cout<<"Enter Octal Number = ";
+    cin>>n;
+
+    cout<<octalToDecimal(n)<<endl;
+    return 0;
+}
