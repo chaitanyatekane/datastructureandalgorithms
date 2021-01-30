17e481bee4085051f7ee93fd7adacfab80dd1c0f
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-30 19:33:58 +0530

    Add files via upload

---
+++ 56.cpp
@@ -0,0 +1,15 @@
+// Write a program to take character input in array if there are no spaces between the characters in word (character array) and then print that
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char arr[100];
+
+    cout<<"Enter Input :- ";
+    cin>>arr;
+
+    cout<<arr;
+    
+    return 0;
+}
