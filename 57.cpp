65d012b16444a6124d2edbcca95ade202d409c7d
Tekane Chaitanya <71593995+chaitanyatekane@users.noreply.github.com>
2021-01-30 19:34:56 +0530

    Write a program to check if character array is palindrome or not

---
+++ 57.cpp
@@ -0,0 +1,34 @@
+// Write a program to check if character array is palindrome or not.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    char arr[n + 1];
+    cin >> arr;
+
+    bool check = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != arr[n - 1 - i])
+        {
+            check = 0;
+            break;
+        }
+    }
+
+    if (check == true)
+    {
+        cout << "Word Is Palindrome" << endl;
+    }
+    else
+    {
+        cout << "Word Is Not A Palindrome" << endl;
+    }
+
+    return 0;
+}
